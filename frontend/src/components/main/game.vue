7391924fcbd4a8b8043a87f6d701f6550a50bd7d
Olli1080 <oliver.zahn2@web.de>
2022-10-27 22:25:36 +0200

    Update game.vue

--- frontend/src/components/main/game.vue
+++ frontend/src/components/main/game.vue
@@ -11,6 +11,8 @@
 <script lang="ts">
 import { defineComponent, ref, onMounted } from 'vue'
 import * as THREE from 'three'
+import { Vector2 } from 'three'
+import { Vec2 } from 'three'
 
 type Directions = 'LEFT' | 'RIGHT' | 'UP' | 'DOWN' | 'NONE'
 const directionMap: { [key: string]: Directions }
@@ -21,13 +23,71 @@ const directionMap: { [key: string]: Directions }
   "s": "DOWN"
 }
 
+function vertexShader()
+{
+  return `
+    varying vec4 pos_v;
+
+    void main() {
+      gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+      pos_v = vec4( position, 1.0 );
+    }
+  `
+}
+
+function fragmentShader()
+{
+  return `
+    varying vec4 pos_v;
+
+    void main() {
+
+      //if (mod(floor(pos_v.x + pos_v.y), 2.f) > 1.f)
+      if (pos_v.x < 0.f)
+      {
+        gl_FragColor = vec4(1,0,0,1);
+      }
+      else
+      {
+        gl_FragColor = vec4(0,1,0,1);
+      }
+    }
+  `
+}
+
+function createBackground(size: Vector2, scene: THREE.Scene)
+{
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const matRed = new THREE.MeshBasicMaterial({ color: 0x99ff99 })
+  const matBlue = new THREE.MeshBasicMaterial({ color: 0x009933 })
+
+  let i = 0
+  for (let y = 0; y < size.y; ++y)
+  {
+    for (let x = 0; x < size.x; ++x, ++i)
+    {
+      if ((x + y) % 2 == 0)
+      {
+        const obj = new THREE.Mesh(geometry, matRed)
+        obj.position.set(x - size.x / 2, y - size.y / 2, -1)
+        scene.add(obj)
+      }
+      else
+      {
+        const obj = new THREE.Mesh(geometry, matBlue)
+        obj.position.set(x - size.x / 2, y - size.y / 2, -1)
+        scene.add(obj)
+      }
+    }
+  }
+}
 
 export default defineComponent({
   setup: () =>
   {
     const renderElement = ref<HTMLDivElement | null>()
 
-    const gridSize = { x: 20, y: 20 }
+    const gridSize = new Vector2(20, 20)
 
     let currentKey = ''
     //TODO:: no direction on start and prompt to choose initial direction
@@ -68,6 +128,35 @@ export default defineComponent({
       return false
     }
 
+    function isInside(position: Vector2, size: Vector2)
+    {
+      //y negative is down
+      const topLeft = new Vector2(-size.x / 2, size.y / 2)
+      const bottomRight = new Vector2(topLeft.x + size.x, topLeft.y - size.y)
+
+      return !(position.x < topLeft.x || position.y >= topLeft.y || position.x >= bottomRight.x || position.y < bottomRight.y)
+    }
+
+    function move(pos: Vector2, direction: Directions)
+    {
+      const out = new Vector2()
+      out.copy(pos)
+
+      switch(direction)
+      {
+        case 'NONE': return out
+        case 'LEFT': out.x -= 1
+          break
+        case 'RIGHT': out.x += 1
+          break
+        case "UP": out.y += 1
+          break
+        case "DOWN": out.y -= 1
+      }
+      return out
+    }
+
+    /*
     function move(head: THREE.Mesh)
     {
       switch(direction)
@@ -82,6 +171,7 @@ export default defineComponent({
         case "DOWN": head.position.y -= 1
       }
     }
+    */
 
     onMounted(() =>
     {
@@ -94,55 +184,34 @@ export default defineComponent({
       //THREE.Matrix4(ratio, height)
 
       const camera = new THREE.OrthographicCamera(width / -2, width / 2, height / 2, height / -2)
+      //const posPro = new THREE.Vector3().setFromMatrixPosition(camera.projectionMatrix)
+      //camera.projectionMatrix.setPosition(posPro.x + 1, posPro.y + 1, posPro.z)
       camera.position.z = 1
       const scene = new THREE.Scene()
 
       //const background =
 
       const geometry = new THREE.BoxGeometry(1, 1, 1)
-      const material = new THREE.MeshNormalMaterial()
+      const material = new THREE.MeshBasicMaterial({ color: 0xff6600 })
 
       const head = new THREE.Mesh(geometry, material)
       scene.add(head)
 
       const renderer = new THREE.WebGLRenderer({ antialias: true })
-      const program = new THREE.WebGLProgram(renderer, 'abc',
-        {
-          vertexShader: `
-                varying vec4 color;
-                gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-                if (floor((position.x + position.y) % 2 == 0))
-                {
-                    color = vec4(1,0,0,1);
-                }
-                else
-                {
-                    color = vec4(0,1,0,1);
-                }
-            `,
-          fragmentShader: `
-                varying vec4 color;
-                gl_FragColor = color;
-            `
-        })
+      createBackground(gridSize, scene)
+      /*
       const backgroundMaterial = new THREE.ShaderMaterial({
-
-        /*uniforms: {
-
-            time: { value: 1.0 },
-            resolution: { value: new THREE.Vector2() }
-
-            },*/
-        //@ts-ignore
-        vertexShader: program.vertexShader,
-        //@ts-ignore
-        fragmentShader: program.fragmentShader
+        vertexShader: vertexShader(),
+        fragmentShader: fragmentShader()
       })
 
       const backgeo = new THREE.BoxGeometry(20, 20, 1)
       const background = new THREE.Mesh(backgeo, backgroundMaterial)
       scene.add(background)
-
+      */
+      const food = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0x996633 }))
+      food.geometry.scale(0.7, 0.7, 1.0)
+      scene.add(food)
 
       renderer.setSize(renderElement.value!.clientWidth, renderElement.value!.clientHeight)
       renderer.setAnimationLoop(animation)
@@ -152,7 +221,14 @@ export default defineComponent({
       {
         if (shouldUpdate(time))
         {
-          move(head)
+          const pos = new Vector2(head.position.x, head.position.y)
+          const newPos = move(pos, direction)
+
+          if (isInside(newPos, gridSize))
+          {
+            head.position.setX(newPos.x)
+            head.position.setY(newPos.y)
+          }
         }
         //mesh.rotation.x = time / 2000;
         //mesh.rotation.y = time / 1000;
